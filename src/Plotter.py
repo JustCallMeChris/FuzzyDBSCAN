acebfd18c9289296ea5ddf8d45f9450e737600a0
Chris Long <chris.long@st.ovgu.de>
2015-05-29 09:15:11 +0200

    Changed some last things. Algorithm seems to be quite well now.

--- src/Plotter.py
+++ src/Plotter.py
@@ -9,38 +9,38 @@ import tempfile as tf
 
 
 def visualizeClustering(data, clustering, eps, minPoints, maxPoints):
-    
-    #data = np.array(data)    
+
+    #data = np.array(data)
     #print type(data)
 
     # Eliminates multiple values
-    unique_labels =set(np.unique(clustering)) 
-    
+    unique_labels =set(np.unique(clustering))
+
     # Color for each cluster
-    colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))  
-    
+    colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
+
     # Colored dot for dot
-    for k, color in zip(unique_labels, colors): 
+    for k, color in zip(unique_labels, colors):
         if k == -1:
             # Noise is shown as black dots
-            color = 'k'   
-            
+            color = 'k'
+
         # True/False matrix to know which dots belongs to cluster
-        class_member_mask = (clustering == k)  
-        # Points of the cluster or noise 
-        xy = data[class_member_mask]   
-              
-        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=color, markeredgecolor='k', markersize=6)   #form, color, bordercolor
-            
+        class_member_mask = (clustering == k)
+        # Points of the cluster or noise
+        xy = data[class_member_mask]
+        # Plots points, last parameters means form, color and bordercolor
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=color, markeredgecolor='k', markersize=6)
+
     plt.title('Fuzzy DBSCAN Clustering')
     plt.xlabel('Eps: '+str(eps)+'    minPtsMin: '+str(minPoints)+'    minPtsMax: '+str(maxPoints))
     #plt.draw()
-    
+
     # Saves the image
     imageName = "fuzzyDBSCAN.png"
     tempPath = tf.mkdtemp()
     plt.savefig(tempPath+os.sep+imageName, dpi=100)
     print("# Image: "+tempPath + os.sep + imageName)
-    
+
     # Shows figure in popup-window
     #plt.show()
