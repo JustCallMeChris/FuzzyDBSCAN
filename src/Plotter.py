16f778f5e8cff5bf3228a3a1cbe2cec3870e6d31
Chris Long <chris.long@st.ovgu.de>
2015-05-29 09:11:03 +0200

    Changed some last things. Algorithm seems to be quite well now.

--- src/Plotter.py
+++ src/Plotter.py
@@ -1,6 +1,6 @@
 # Show a nice picture ;)
-# Input: Numpy matrix (not yet) of data points (data) and
-# list of cluster ids for the given data points (clustering).
+# Input: Numpy array of data points (data) and
+# List of cluster labels for the given data points (clustering).
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,21 +10,28 @@ import tempfile as tf
 
 def visualizeClustering(data, clustering, eps, minPoints, maxPoints):
     
-    data = np.array(data)
+    #data = np.array(data)    
+    #print type(data)
+
+    # Eliminates multiple values
+    unique_labels =set(np.unique(clustering)) 
     
-    unique_labels =set(np.unique(clustering)) # eliminates multiple values
-    colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels))) # color for each cluster 
+    # Color for each cluster
+    colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))  
     
-    for k, color in zip(unique_labels, colors): # colored dot for dot
+    # Colored dot for dot
+    for k, color in zip(unique_labels, colors): 
         if k == -1:
-            color = 'k'   # noise is shown as black dots
-
-        class_member_mask = (clustering == k)   #true/false matrix to know which dots belongs to cluster
-
-        xy = data[class_member_mask] #points of the cluster or noise    
+            # Noise is shown as black dots
+            color = 'k'   
+            
+        # True/False matrix to know which dots belongs to cluster
+        class_member_mask = (clustering == k)  
+        # Points of the cluster or noise 
+        xy = data[class_member_mask]   
               
-        
         plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=color, markeredgecolor='k', markersize=6)   #form, color, bordercolor
+            
     plt.title('Fuzzy DBSCAN Clustering')
     plt.xlabel('Eps: '+str(eps)+'    minPtsMin: '+str(minPoints)+'    minPtsMax: '+str(maxPoints))
     #plt.draw()
@@ -35,10 +42,5 @@ def visualizeClustering(data, clustering, eps, minPoints, maxPoints):
     plt.savefig(tempPath+os.sep+imageName, dpi=100)
     print("# Image: "+tempPath + os.sep + imageName)
     
-    # Prints fullpath of the file
-    #full_path = os.path.realpath(__file__)
-    #path, f = os.path.split(full_path)
-    #print(path + os.sep + imageName) 
-    
-    # Shows figure in separate popup-window
+    # Shows figure in popup-window
     #plt.show()
